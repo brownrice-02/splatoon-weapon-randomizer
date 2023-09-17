90850ae0cf8f56a8ac834b00fabbdeb0328f1cc6
brownrice-02 <vickylee.g123@gmail.com>
2023-09-17 19:58:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Splatoon</h1>
+      <h1>私房武器抽選</h1>
+      <div className="card">
+        <button>隨機分配武器</button>
+      </div>
+      <div>
+        <div>武器</div>
+        <div>武器</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
