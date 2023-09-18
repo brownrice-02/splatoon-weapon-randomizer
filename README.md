ec2ea91d1c0cdfc5578f0e11c7a8ec418de7b9e1
brownrice-02 <vickylee.g123@gmail.com>
2023-09-18 17:56:59 +0800

    add initialized random weapon picking logic

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # splatoon-weapon-randomizer
 在 Splatoon 與朋魷們玩私房模式時，能夠抽選隨機武器，使遊戲更具挑戰性和樂趣～
-# splatoon-weapon-randomizer
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import "./App.css";
+import weaponsData from "./weaponsData/weapons.json";
 
 function App() {
+  const [randomWeapon, setRandomWeapon] = useState(null);
+
+  const pickRandomWeapon = () => {
+    const weapons = weaponsData.weapons;
+    // 從這些武器中隨機選擇一個
+    const randomIndex = Math.floor(Math.random() * weapons.length);
+    const selectedWeapon = weapons[randomIndex];
+
+    setRandomWeapon(selectedWeapon); // 更新隨機武器
+  };
   return (
     <>
       <h1>Splatoon</h1>
-      <h1>私房武器抽選</h1>
+      <h2>私房武器抽選</h2>
       <div className="card">
-        <button>隨機分配武器</button>
+        <button onClick={pickRandomWeapon}>隨機分配武器</button>
       </div>
-      <div>
-        <div>武器</div>
-        <div>武器</div>
-      </div>
+      {randomWeapon && (
+        <div>
+          <div>
+            {randomWeapon.name.tw}({randomWeapon.name.jp})
+          </div>
+          <img src={randomWeapon.image} alt={randomWeapon.name.tw} />
+        </div>
+      )}
     </>
   );
 }
