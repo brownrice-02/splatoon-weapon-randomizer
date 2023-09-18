a4b065dc7431e2417c40917eb47c0a7ec11ca791
brownrice-02 <vickylee.g123@gmail.com>
2023-09-18 18:38:13 +0800

    can pick 8 weapons

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,55 @@ import "./App.css";
 import weaponsData from "./weaponsData/weapons.json";
 
 function App() {
-  const [randomWeapon, setRandomWeapon] = useState(null);
+  const [randomWeapons, setRandomWeapons] = useState([]);
+  const numberOfWeaponsToPick = 8; // 要選擇的武器數量
 
-  const pickRandomWeapon = () => {
+  const pickRandomWeapons = () => {
     const weapons = weaponsData.weapons;
-    // 從這些武器中隨機選擇一個
-    const randomIndex = Math.floor(Math.random() * weapons.length);
-    const selectedWeapon = weapons[randomIndex];
+    const selectedWeapons = [];
 
-    setRandomWeapon(selectedWeapon); // 更新隨機武器
+    while (selectedWeapons.length < numberOfWeaponsToPick) {
+      const randomIndex = Math.floor(Math.random() * weapons.length);
+      const selectedWeapon = weapons[randomIndex];
+
+      // 確保選擇的武器不重複
+      if (!selectedWeapons.some((w) => w.name.tw === selectedWeapon.name.tw)) {
+        selectedWeapons.push(selectedWeapon);
+      }
+    }
+
+    setRandomWeapons(selectedWeapons); // 更新隨機武器
   };
+
   return (
     <>
       <h1>Splatoon</h1>
       <h2>私房武器抽選</h2>
       <div className="card">
-        <button onClick={pickRandomWeapon}>隨機分配武器</button>
+        <button onClick={pickRandomWeapons}>隨機分配武器</button>
       </div>
-      {randomWeapon && (
-        <div>
-          <div>
-            {randomWeapon.name.tw}({randomWeapon.name.jp})
+      {randomWeapons.length > 0 && (
+        <div className="weapons-container">
+          <div className="left-container">
+            {randomWeapons.slice(0, 4).map((weapon, index) => (
+              <div key={index} className="weapon">
+                <div>
+                  {weapon.name.tw} ({weapon.name.jp})
+                </div>
+                <img src={weapon.image} alt={weapon.name.tw} />
+              </div>
+            ))}
           </div>
-          <img src={randomWeapon.image} alt={randomWeapon.name.tw} />
+          <div className="right-container">
+            {randomWeapons.slice(4).map((weapon, index) => (
+              <div key={index} className="weapon">
+                <div>
+                  {weapon.name.tw} ({weapon.name.jp})
+                </div>
+                <img src={weapon.image} alt={weapon.name.tw} />
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </>
