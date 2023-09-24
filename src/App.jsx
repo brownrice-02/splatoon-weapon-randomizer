6a38336d1a60eedc626c1851a404c00859103cf4
brownrice-02 <vickylee.g123@gmail.com>
2023-09-24 17:18:00 +0800

    back to only one button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import weaponsData from "./weaponsData/weapons.json";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
 
 function App() {
   const [randomWeapons, setRandomWeapons] = useState([]);
-  const [allowDuplicates, setAllowDuplicates] = useState(false); // 是否允許重複選擇
   const numberOfWeaponsToPick = 8; // 要選擇的武器數量
-
-  const toggleDuplicates = () => {
-    setAllowDuplicates((prev) => !prev);
-  };
 
   const pickRandomWeapons = () => {
     const weapons = weaponsData.weapons;
@@ -23,10 +15,7 @@ function App() {
       const selectedWeapon = weapons[randomIndex];
 
       // 檢查是否允許重複選擇或選擇的武器不在已選擇列表中
-      if (
-        allowDuplicates ||
-        !selectedWeapons.some((w) => w.name.tw === selectedWeapon.name.tw)
-      ) {
+      if (!selectedWeapons.some((w) => w.name.tw === selectedWeapon.name.tw)) {
         selectedWeapons.push(selectedWeapon);
       }
     }
@@ -41,21 +30,6 @@ function App() {
         <h1>私房武器抽選</h1>
       </div>
       <div className="picker">
-        <FormControl
-          variant="standard"
-          sx={{ m: 1, minWidth: 120 }}
-          size="small"
-        >
-          <Select
-            labelId="duplicates-label"
-            id="duplicates-select"
-            value={allowDuplicates}
-            onChange={toggleDuplicates}
-          >
-            <MenuItem value={false}>不重複</MenuItem>
-            <MenuItem value={true}>可重複</MenuItem>
-          </Select>
-        </FormControl>
         <button onClick={pickRandomWeapons}>隨機分配武器</button>
       </div>
       {randomWeapons.length > 0 && (
